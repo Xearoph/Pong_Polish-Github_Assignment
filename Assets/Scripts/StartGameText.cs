6fcad079ff233c1c054ea187a68335dc9f6846cc
Mister van Beek <87307361+Xearoph@users.noreply.github.com>
2022-10-04 22:07:09 +0200

    Update StartGameText.cs

--- Assets/Scripts/StartGameText.cs
+++ Assets/Scripts/StartGameText.cs
@@ -9,7 +9,7 @@ public class StartGameText : MonoBehaviour
         ballMovement = GameObject.FindGameObjectWithTag("Player").GetComponent<BallMovement>();
         playText = GameObject.FindGameObjectWithTag("PlayText");
     }
-
+    // Update is called every frame
     void Update()
     {
         if (ballMovement.isPlay) playText.SetActive(false);
