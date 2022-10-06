a59fe2172b17756edf4d102084effa80ef058f2c
sem Gerritsen <38108953+semgerritsen@users.noreply.github.com>
2022-10-06 14:11:58 +0200

    werkende goal explosie
    zodra de bal achter de speler komt word het particle system geactiveerd

--- Assets/Scripts/GoalExplosion.cs
+++ Assets/Scripts/GoalExplosion.cs
@@ -5,24 +5,38 @@ public class GoalExplosion : MonoBehaviour
 {
 
     GameObject objToSpawn;
+    Transform posBall;
+    Transform leftPLayer;
+    Transform rightPLayer;
     // Start is called before the first frame update
     void Start()
     {
-        objToSpawn = new GameObject("particlesystem");
-        objToSpawn.AddComponent<ParticleSystem>();
+        posBall = GameObject.FindGameObjectWithTag("Player").GetComponent<Transform>();
+        leftPLayer = GameObject.FindGameObjectWithTag("PlayerLeft").GetComponent<Transform>();
+        rightPLayer = GameObject.FindGameObjectWithTag("PlayerRight").GetComponent<Transform>();
+        objToSpawn = new GameObject("empty object");
 
     }
 
     // Update is called once per frame
     void Update()
     {
-
-    }
-    void OnCollision2D(Collision2D collision)
-    {
-        if (collision.gameObject.tag == "Boundleft" || collision.gameObject.tag == "BoundRight")
+        
+        if (posBall.position.x >  rightPLayer.position.x+2)
         {
-            
+            objToSpawn.AddComponent<ParticleSystem>();
+            Destroy(objToSpawn,3);
         }
+        else if (posBall.position.x < leftPLayer.position.x-2)
+        {
+            objToSpawn.AddComponent<ParticleSystem>();
+            Destroy(objToSpawn,3);
+        }
+        if (objToSpawn == null)
+        {
+            objToSpawn = new GameObject("empty object");
+        }
+        
     }
 }
+
