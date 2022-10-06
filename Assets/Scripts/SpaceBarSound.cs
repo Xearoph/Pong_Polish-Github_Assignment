03fea97b08dc340559f67e65e5fb85bad346d525
Liam053 <Liam.kleinhalle@ziggo.nl>
2022-10-06 13:19:58 +0200

    Audioclip toegevoegd/script toegevoegd
    ik heb het script gemaakt die SpaceBarSound heet en ook op de gamecontroller gezet
    
    ik heb de audioclip op het script gezet in de inspector

---
+++ Assets/Scripts/SpaceBarSound.cs
@@ -0,0 +1,20 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpaceBarSound : MonoBehaviour
+{
+    public AudioClip[] soundFX;
+
+
+    private void Update()
+    {
+        if (Input.GetButtonDown("Space"))
+        {
+           
+        }
+    }
+
+
+}
