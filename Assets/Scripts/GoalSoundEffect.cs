0436c9bfb814fbec81f89eb79bd2856559b69757
Luuk-Raijmakers <92307215+Luuk-Raijmakers@users.noreply.github.com>
2022-10-06 15:27:46 +0200

    Complete redone the code and files
    Added:
    -Redone Goal Sound Effect script
    -Removed 3 audiofiles
    -Added Resource map

--- Assets/Scripts/GoalSoundEffect.cs
+++ Assets/Scripts/GoalSoundEffect.cs
@@ -5,39 +5,27 @@ using UnityEngine;
 //Writen by Luuk
 public class GoalSoundEffect : MonoBehaviour
 {
-    public GameObject wallLeft;
-    public GameObject wallRight;
-
     private AudioSource audioS;
-    public AudioClip[] goalSoundEffects;
+    public GameObject player;
     private float volume = 0.6f;
 
     private void Start()
     {
-        //Create temporary component on this gameobject
-        wallLeft.AddComponent<GoalSoundEffect>();
-        wallRight.AddComponent<GoalSoundEffect>();
-
+        //Add temporary audiosource component
         audioS = gameObject.AddComponent<AudioSource>();
-        //Set the volume of the audioSource the same as the float volume
+        player.AddComponent<GoalSoundEffect>();
+        //Load the audioclip from the resource map
+        //Change the volume of the temporary audiosource
         audioS.volume = volume;
     }
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        //If the ball collide with the WallLeft or WallRight...
-        if (collision.gameObject.tag == "Player")
+        audioS.clip = Resources.Load<AudioClip>("Audio/GoalSoundEffect");
+
+        if (collision.gameObject.tag == "BoundLeft" || collision.gameObject.tag == "BoundRight")
         {
-            //Get a random audioclip and play that via the audioSource
-            audioS.clip = GetRandomClip();
-            //Play the audioclip via the audioSource
             audioS.Play();
         }
-    }
-
-    private AudioClip GetRandomClip()
-    {
-        //Select a random audiofile from 0 to the amount that is selected in the inspector. And then return that file so that it can be played by the audiosource
-        return goalSoundEffects[Random.Range(0, goalSoundEffects.Length)];
     }
 }
