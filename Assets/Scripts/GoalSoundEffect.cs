b3e4fe2448c88c56884d77e068c2815266537e91
Luuk-Raijmakers <92307215+Luuk-Raijmakers@users.noreply.github.com>
2022-10-06 15:37:34 +0200

    Comments
    Added:
    -Made the comments better

--- Assets/Scripts/GoalSoundEffect.cs
+++ Assets/Scripts/GoalSoundEffect.cs
@@ -11,26 +11,32 @@ public class GoalSoundEffect : MonoBehaviour
 
     private void Start()
     {
+        //the gameobject is whatever gameobject has the tag "player"
         player = GameObject.FindGameObjectWithTag("Player");
+
         //Add temporary audiosource component
         audioS = gameObject.AddComponent<AudioSource>();
 
+        //If the player has no goalsoundeffect script
         if(!player.TryGetComponent<GoalSoundEffect>(out _))
         {
+            //Added the goalsoundeffect script
             player.AddComponent<GoalSoundEffect>();
         }
 
-        //Load the audioclip from the resource map
         //Change the volume of the temporary audiosource
         audioS.volume = volume;
     }
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
+        //Load the audioclip from the resource map
         audioS.clip = Resources.Load<AudioClip>("Audio/GoalSoundEffect");
 
+        //If the player collide with the wall...
         if (collision.gameObject.tag == "BoundLeft" || collision.gameObject.tag == "BoundRight")
         {
+            //Play the soundeffect
             audioS.Play();
         }
     }
