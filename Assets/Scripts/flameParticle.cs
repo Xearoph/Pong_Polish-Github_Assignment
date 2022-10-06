a6b2870ede8b22f29770b863422b6bd48b16726e
kevin gg <kevin204455@gmail.com>
2022-10-06 13:51:32 +0200

    Trail Effect
    Added a trail effect that follows the ball

--- Assets/Scripts/flameParticle.cs
+++ Assets/Scripts/flameParticle.cs
@@ -5,16 +5,20 @@ using UnityEngine;
 public class flameParticle : MonoBehaviour
 {
     private GameObject ballObject;
+
+
     // Start is called before the first frame update
     void Start()
     {
         ballObject = GameObject.FindWithTag("Player");
-        ballObject.AddComponent<ParticleSystem>();
+        ballObject.AddComponent<TrailRenderer>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
+        ballObject.GetComponent<TrailRenderer>().material.color = Color.cyan;
+        ballObject.GetComponent<TrailRenderer>().time = 1.0f;
     }
 }
