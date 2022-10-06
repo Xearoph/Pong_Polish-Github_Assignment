3d849e5e4d6489bd8b77f2afdfb072730f17e468
Olivier <olivierhoogers@gmail.com>
2022-10-06 14:28:27 +0200

    iets veranderd
    verder niks speciaals onnodige niet werkende dingen removed.

--- Assets/Scripts/CameraShake.cs
+++ Assets/Scripts/CameraShake.cs
@@ -4,12 +4,14 @@ using UnityEngine;
 
 public class CameraShake : MonoBehaviour
 {
+    public GameObject ball;
+    private Vector2 ballPosition;
     // the higher the shakepower the more intense.
     public float shakePower = 1f;
     //the higher the shakelength the longer it lasts.
     public float shakeLength = 1f;
     // put camera on here.
-    public Transform camera;
+    public Transform mainCamera;
     // boolean to check if its still shaking or not.
     public bool isShaking = false;
 
@@ -18,25 +20,40 @@ public class CameraShake : MonoBehaviour
     void Start()
     {
         // save the camera's startposition.
-        cameraStartPosition = camera.transform.position;
-
+        cameraStartPosition = mainCamera.transform.position;
+        
     }
 
     // Update is called once per frame
     void Update()
     {
+      
+        ballPosition = ball.transform.position;
+        if(ballPosition.x >= 16.8 || ballPosition.x <= -16.8)
+        {
+            print("hi");
+            isShaking = true;
+        }
         if (isShaking)
-        {
-            // calculate random values for the x & y axis.
-            float x = Random.Range(-1f, 1f) * shakePower;
-            float y = Random.Range(-1f, 1f) * shakePower;
-            camera.transform.position = new Vector2(x, y);
-        }
-        else
-        {
-            isShaking = false;
-            shakeLength = 1;
+        { 
+          float durationTime =- Time.deltaTime * shakeLength;
+            if (durationTime < 0)
+            {
+                Debug.Log("shake");
 
+                // calculate random values for the x & y axis.
+                float x = Random.Range(-1f, 1f) * shakePower;
+                float y = Random.Range(-1f, 1f) * shakePower;
+                mainCamera.transform.position = new Vector2(x, y);
+            }
+            else
+            {
+                isShaking = false;
+
+            }
         }
+   
     }
+
+    
 }
