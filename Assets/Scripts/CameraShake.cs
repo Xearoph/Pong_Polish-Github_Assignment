9d8300b550f59e7de2f0a364a945a505538abb29
Olivier <olivierhoogers@gmail.com>
2022-10-11 18:37:29 +0200

    Camerashake werkt
    Camerashake werkt alleen is de shakelength nog te lang en wil ik eerst even de main pullen om te checken of uberhaupt mijn script zal werken met de rest.

--- Assets/Scripts/CameraShake.cs
+++ Assets/Scripts/CameraShake.cs
@@ -8,7 +8,7 @@ public class CameraShake : MonoBehaviour
     private Vector2 ballPosition;
     // the higher the shakepower the more intense.
     public float shakePower = 1f;
-    //the higher the shakelength the longer it lasts.
+    // time the camera shakes.
     public float shakeLength = 1f;
     // put camera on here.
     public Transform mainCamera;
@@ -21,38 +21,45 @@ public class CameraShake : MonoBehaviour
     {
         // save the camera's startposition.
         cameraStartPosition = mainCamera.position;
-        
+       
     }
 
+    
     // Update is called once per frame
     void Update()
     {
-      
+       
+
         ballPosition = ball.transform.position;
         if(ballPosition.x >= 16.8 || ballPosition.x <= -16.8)
         {
             print("hi");
             isShaking = true;
         }
-        if (isShaking)
-        { 
-          float durationTime =- Time.deltaTime * shakeLength;
-            if (durationTime < 0)
-            {
-                Debug.Log("shake");
-
-                // calculate random values for the x & y axis.
-                float x = Random.Range(-1f, 1f) * shakePower;
-                float y = Random.Range(-1f, 1f) * shakePower;
-                mainCamera.transform.position = new Vector3(x, y, -10);
-            }
-            else
-            {
-                isShaking = false;
-
-            }
+        
+        if(isShaking)
+        {
+            StartCoroutine(Camerashake());
         }
    
+    }
+
+    public IEnumerator Camerashake()
+    {
+       float timeElapsed = 0f;
+      while(timeElapsed < shakeLength)
+       { 
+            Debug.Log("shake");
+
+            // calculate random values for the x & y axis.
+            float x = Random.Range(-1f, 1f) * shakePower;
+            float y = Random.Range(-1f, 1f) * shakePower;
+            mainCamera.transform.position = new Vector3(x, y, -10);
+           timeElapsed += Time.deltaTime * 1;
+            yield return 0;
+            isShaking = false;          
+      }
+ 
     }
 
     
