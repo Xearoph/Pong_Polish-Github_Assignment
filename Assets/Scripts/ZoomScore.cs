5c9b3cfcc9b88884caa101652dbce03b6fdd5636
WillemijnSter <Willemijn.sterken@gmail.com>
2022-11-01 09:28:01 +0100

    Name script change
    change of a script name

--- Assets/Scripts/ZoomScore.cs
+++ Assets/Scripts/ZoomScore.cs
@@ -3,28 +3,28 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class ZoomScore : MonoBehaviour
-{
+{   
+    //camera, ball and speed declaration
     GameObject ball;
     Camera thisCamera;
-    private float speed = 17f;
+    [SerializeField] private float speed = 17f;
     void Start()
-    {   
+    {   //asign ball and camera
         ball = GameObject.FindGameObjectWithTag("Player");
-        
         thisCamera = Camera.main;
     }
 
-    private void OnTriggerEnter2D(Collider2D collision)
+    private void OnTriggerEnter2D(Collider2D collision) //when something triggers the ball then start a coroutine
     {
         StartCoroutine(ThisCoroutine());
     }
 
     IEnumerator ThisCoroutine()
     {
-        Quaternion standardPosition = thisCamera.transform.rotation;
-        float target = 0f;
+        Quaternion standardPosition = thisCamera.transform.rotation; //standardposition is current position of the camera
+        float target = 0f; //target is a float and is zero.
 
-        for(int i = 0; i < 30; i++) 
+        for(int i = 0; i < 30; i++) //this part still has to be commented
         {
             Vector3 targetDirection = ball.transform.position - thisCamera.transform.position;
             float singleStep = speed * Time.deltaTime;
